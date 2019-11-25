36dfbe9e4e53a0cd7e3d3de9cb61953deb5b9bfd
khd2019 <57554278+khd2019@users.noreply.github.com>
2019-11-25 15:55:44 +0900

    Create data_loader.py

---
+++ Mammo/evaluation/data_loader.py
@@ -0,0 +1,38 @@
+import os
+import numpy as np
+import sys
+import time
+import nsml
+from nsml.constants import DATASET_PATH
+
+
+def test_data_loader (root_path): ## main loader를 사용해도 무관하나/ 현재 main 코드에서는 data와 label을 동시에 읽기 때문에 따로 만들어줌
+    data_path = [] # data path 저장을 위한 변수
+    labels=[] # 테스트 id 순서 기록
+    ## 하위 데이터 path 읽기
+    for dir_name,_,_ in os.walk(root_path):
+        try: 
+            data_id = dir_name.split('/')[-1]
+            int(data_id)    
+        except: pass
+        else: 
+            data_path.append( dir_name )
+            labels.append(int(data_id[0]))
+    
+    ## 데이터만 읽기
+    data = [] # img저장을 위한 list
+    for d_path in data_path:
+        sample = np.load(d_path+'/mammo.npz')['arr_0']
+        data.append(sample)
+    data = np.array(data) ## list to numpy
+    return data, labels
+
+
+def feed_infer(output_file, infer_func):
+    data, labels = test_data_loader(os.path.join(DATASET_PATH,'test'))
+    result = infer_func(data)
+    print('write output')
+    with open(output_file, 'wt') as file_writer:
+        file_writer.write('\n'.join('%s, %s' % x for x in zip(result, labels)))
+    if os.stat(output_file).st_size ==0:
+        raise AssertionError('output result of inference is nothing')
